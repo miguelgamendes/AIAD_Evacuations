427a8f644d6b39f4eef20c8629b9f1e04c3172e7
Miguel Mendes <zephiro309@gmail.com>
2015-12-02 12:15:15 +0000

    Stuff

--- src/agent/selfish/SelfishBDI.java
+++ src/agent/selfish/SelfishBDI.java
@@ -1,10 +1,19 @@
 package agent.selfish;
 
 import agent.BaseBDI;
+import jadex.bdiv3.BDIAgent;
+import jadex.bdiv3.annotation.Capability;
+import jadex.extension.envsupport.math.Vector2Int;
+import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentBody;
+import movement.MovementCapability;
 
 /**
  * Created by joao on 25-11-2015.
  */
+
+@Agent
 public class SelfishBDI extends BaseBDI {
+
 
 }
